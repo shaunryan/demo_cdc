0f0fd7199d8b1ace8498325c6608b30b3ca553dc
shaunryan <shaunryan>
2023-01-22 12:43:06 +0000

    running on databricks

--- notebook_main.py
+++ notebook_main.py
@@ -8,8 +8,6 @@ from yetl.flow import Timeslice, OverwriteSave
 from yetl.workflow import multithreaded as yetl_wf
 import yaml
 
-# timeslice = Timeslice(year=2021, month=1, day=1)
-# timeslice = Timeslice(year=2021, month=1, day=2)
 timeslice = Timeslice(year="*", month="*", day="*")
 project = "demo_cdc"
 maxparallel = 2
@@ -27,4 +25,33 @@ yetl_wf.load(project, tables, landing_to_raw, timeslice, OverwriteSave, maxparal
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC drop database demo_cdc_landing cascade
+# MAGIC 
+# MAGIC select * from demo_cdc_raw.customer_details_history
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC 
+# MAGIC select * from demo_cdc_raw.customer_details
+
+# COMMAND ----------
+
+dbutils.notebook.exit("Done")
+
+# COMMAND ----------
+
+
+
+def clear_down():
+  try:
+    spark.sql("drop database demo_cdc_landing cascade")
+  except:
+    pass
+  try:
+    spark.sql("drop database demo_cdc_raw cascade")
+  except:
+    pass
+  dbutils.fs.rm("/mnt/datalake/data/delta_lake/demo_cdc_landing/", True)
+  dbutils.fs.rm("/mnt/datalake/data/delta_lake/demo_cdc_raw/", True)
+
+clear_down()
